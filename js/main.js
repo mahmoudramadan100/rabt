5d59ad74226df86a9ee3543ad2bf482fb095f2b2
mahmoud ramadan <mahmoudr03@gmail.com>
2022-11-30 15:55:07 +0200

    done code

--- js/main.js
+++ js/main.js
@@ -15,21 +15,21 @@ $(function() {
  });
 });
 
-$(document).ready(()=>{
-  let isMenuAlreadyOpen = false;
-  $('.nav-mobile').on('click',()=>{
-        $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
-        isMenuAlreadyOpen = !isMenuAlreadyOpen
-  })
-});
+// $(document).ready(()=>{
+//   let isMenuAlreadyOpen = false;
+//   $('.nav-mobile').on('click',()=>{
+//         $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
+//         isMenuAlreadyOpen = !isMenuAlreadyOpen
+//   })
+// });
 
 
 
 
 
 
-$(document).ready(function(){
-  $(".unread").click(function(){
-    $(".dropdown-menu-up").toggle();
-  });
-});
+// $(document).ready(function(){
+//   $(".unread").hover(function(){
+//     $(".dropdown-menu-up").toggle();
+//   });
+// });
